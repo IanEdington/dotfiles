8dbe8d378d382c212c249fa50150100d374d87af
Ian Edington <IanEdington@gmail.com>
2015-11-08 13:08:57 -0500

    reorganized to include all of the .config folder

---
+++ bash/bookmarks.bash
@@ -0,0 +1,6 @@
+# Bookmarks
+
+alias udacity-coursework='cd ~/Dropbox/dev/udacity_coursework/'
+alias udacity-project='cd ~/Dropbox/dev/udacity_project/'
+alias playground='cd ~/Dropbox/dev/playground/'
+alias eyecarepd='cd ~/Dropbox/dev/Instuctio/eyecarepd'
