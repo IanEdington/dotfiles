3261b1c88c94829700f207a3a9dce2f78526d6d7
Ian Edington <IanEdington@gmail.com>
2018-03-14 15:56:51 -0400

    Chage default gui editor to VS code

--- zsh/settings/git.zsh
+++ zsh/settings/git.zsh
@@ -8,7 +8,7 @@ __git_files () {
 }
 
 alias gi='$EDITOR .gitignore'
-alias egs='idea `git status --short -- . | awk '"'"'{print $2}'"'"'`'
+alias egs='code `git status --short -- . | awk '"'"'{print $2}'"'"'`'
 alias egh='$EDITOR `git diff --name-only HEAD^`'
 
 # Git flow
