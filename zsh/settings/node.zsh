8efd095e5cb757230682e05a4a3b04d67a031927
Ian Edington <IanEdington@gmail.com>
2020-12-06 13:46:19 -0500

    some more changes... I've stopped trying to keep good notes

--- zsh/settings/node.zsh
+++ zsh/settings/node.zsh
@@ -1,5 +1,6 @@
 export NODE_PARAMS='--max-old-space-size=8192'
 
 export NVM_DIR="$HOME/.cache/nvm"
-export NODE_VERSIONS="$NVM_DIR/versions"
+export NODE_VERSIONS="$NVM_DIR/versions/node"
+export NODE_VERSION_PREFIX="v"
 source "/usr/local/opt/nvm/nvm.sh"
--- zsh/settings/yarn.zsh
+++ zsh/settings/yarn.zsh
@@ -1,2 +1,14 @@
 YARN_CACHE_FOLDER=~/.cache/yarn
+YARN_CONFIG_FOLDER=~/.config/yarn
+#
+# yarn list all linked packages for project
+yllc() {
+  find node_modules -type l -maxdepth 3 | grep -v .bin
+}
+
+# yarn list all links
+yll() {
+  linkDir="$YARN_CONFIG_FOLDER/link"
+  find $linkDir -maxdepth 2 -type l | xargs realpath -s --relative-to $linkDir
+}
 
