f842c9cf5f90307561f56f13c874719a0d94ccd0
Ian Edington <ian.edington@gmail.com>
2024-04-25 21:32:38 -0400

    Lots of changes :O

--- linux/programs.sh
+++ linux/programs.sh
@@ -6,4 +6,40 @@ IFS=$'\n\t'
 sudo apt install \
     direnv \
     tig \
-    vim \
+    vim
+
+# sudo apt install screenkey \
+#                  vlc vlc-plugin-fluidsynth vlc-plugin-jack vlc-plugin-svg
+#                  fasd
+#                  fzy
+#                  jq
+#                  gpg
+
+# desktop tools
+# flatpak install flathub org.signal.Signal
+# flatpak install flathub com.slack.Slack
+flatpak install flathub it.mijorus.smile
+
+
+
+    Foliate (ebook reader app with lots of options)
+    Drawing (a simple ‘Microsoft Paint’ alternative)
+    Lollypop (a desktop music player with neat UI)
+    Authenticator (2FA code generator that supports lots of services)
+    ImCompressor (a lossless image optimisation tool)
+
+    GIMP (decent Photoshop alternative)
+    Kdenlive (versatile video editor)
+    Visual Studio Code (powerful code editing)
+    Blender (state-of-the-art graphics creation)
+    Geary (modern email client)
+    Chromium (web browser)
+
+And there’s a swathe of other non-free software you may want to use too, including:
+
+    Google Chrome (a unicorn that sings)
+    Spotify (music streaming service)
+    Skype (messaging service)
+    Steam (much loved gaming client)
+    Discord (voice chat)
+    Slack (team messaging)
---
+++ notes.md
@@ -0,0 +1,43 @@
+# things I want to do
+
+- quickadd to workflowy
+- auto start `start-keybaord`
+- i3wm: open notificaions when clicked
+
+intellij:
+- cmd+= - larger text
+- cmd+- - smaller text
+
+vim:
+- `md` files: stop tabing in when `-` exists on the previous line
+
+
+# scoll speed
+decrease scroll speed as per this articel: https://forums.linuxmint.com/viewtopic.php?t=375106
+
+xinput --set-prop <ID> "libinput Scrolling Pixel Distance" 30
+
+
+# framework suggested
+
+https://github.com/FrameworkComputer/linux-docs/blob/main/LinuxMint21-1-Manual-Setup-13thGen.md
+
+# Keyboard using KMonad
+
+
+# macos keyboard
+https://github.com/petrstepanov/gnome-macos-remap/blob/master/bin/install-dconf.sh
+
+
+# Apps to install
+
+- ranger
+- alfred
+    - look into https://github.com/albertlauncher/albert/issues/123
+
+
+# Apps installed so far
+autokey-gtk
+
+pip
+speech-dispatcher
