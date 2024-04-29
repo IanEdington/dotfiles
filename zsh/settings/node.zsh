815ec632d083ea7b5e680276f9c7c186f784cc2c
Ian Edington <ian.edington@gmail.com>
2024-04-28 20:30:57 -0400

    Set up NVM

--- zsh/settings/node.zsh
+++ zsh/settings/node.zsh
@@ -1,8 +1,8 @@
-export NODE_PARAMS='--max-old-space-size=8192'
-
+# nvm
 export NVM_DIR="$HOME/.cache/nvm"
-export NODE_VERSIONS="$NVM_DIR/versions/node"
-export NODE_VERSION_PREFIX="v"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 if [[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ]]; then
     source "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
 fi
