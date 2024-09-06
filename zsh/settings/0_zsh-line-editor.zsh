1a5cef7bfe56e5258e738591b30cb60faf180c0b
Ian Edington <ian.edington@gmail.com>
2024-09-06 14:01:17 -0400

    Delete unused code

--- zsh/settings/0_zsh-line-editor.zsh
+++ zsh/settings/0_zsh-line-editor.zsh
@@ -9,15 +9,6 @@ export EDITOR=vim
 export VISUAL=vim
 export IDE=idea
 
-# TODO change to cmd-< cmd->
-# bindkey '^a' beginning-of-line
-# bindkey '^e' end-of-line
-
-# Make numpad enter work
-# bindkey -s "^[Op" "0"
-# bindkey -s "^[Ol" "."
-# bindkey -s "^[OM" "^M"
-
 # Kill The Lag [http://dougblack.io/words/zsh-vi-mode.html]
 # note: This can result in issues with other terminal commands that depended on this delay. If you have issues try raising the delay.
 export KEYTIMEOUT=1
@@ -29,16 +20,7 @@ bindkey '^P' up-history
 bindkey '^N' down-history
 bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
-# bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
-
-# function zle-line-init zle-keymap-select {
-#     VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
-#     RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}$(git_custom_status) $EPS1"
-#     zle reset-prompt
-# }
-# zle -N zle-line-init
-# zle -N zle-keymap-select
 
 # mimic vim functions
 alias :q='exit'
