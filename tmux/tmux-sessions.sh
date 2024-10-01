200b0fa023c0886b1a2d592aebe4f0d35b40c417
Ian Edington <ian.edington@gmail.com>
2024-10-01 13:39:03 -0400

    update tmux script examples

--- tmux/tmux-sessions.sh
+++ tmux/tmux-sessions.sh
@@ -8,15 +8,22 @@ function create-new-session() {
     tmux new-session -d -s $1 -c "$2/$1" || true
 }
 
+tmux new-session -d -s anything -c ~ || true
 create-new-session dotfiles ~/code
-create-new-session backend ~/code/faire
-create-new-session datagrip ~/code/faire
-create-new-session backend2 ~/code/faire
-create-new-session mjml-react ~/code/faire
-create-new-session web-email-server ~/code/faire
-create-new-session platform-configs ~/code/faire
+create-new-session secure-gpo-ca ~/code/gpo
+create-new-session gpo-ca ~/code/gpo
+create-new-session gpo ~/code/gpo
+create-new-session data ~/code/gpo
+create-new-session gppackagist ~/code
+create-new-session platform-configs ~/code/gpo
+
+# servers
+tmux new-session -d -s gpo-servers -n 'prod1' 'mosh prod1'
+tmux new-window -t gpo-servers:2 -n 'prod2' 'mosh prod2'
+tmux new-window -t gpo-servers:3 -n 'data1' 'mosh data1'
+tmux new-window -t gpo-servers:4 -n 'non-prod' 'mosh non-prod'
 
 if [ -z ${TMUX+x}  ]; then
     echo "attach to session";
-    tmux attach -t backend
+    tmux attach -t dotfiles
 fi
