e6d93801e4d37855f86f904b7ae05214b011f922
Ian Edington <ian.edington@gmail.com>
2024-10-25 10:40:41 -0400

    update prompt to always show time

--- zsh/settings/prompt.zsh
+++ zsh/settings/prompt.zsh
@@ -5,24 +5,25 @@ zstyle ':prezto:module:editor:info:keymap:primary' format '%B%F{blue}$%f%b'
 RPROMPT=""
 
 function prompt_paradox_print_elapsed_time() {
-  local end_time=$(( SECONDS - _prompt_paradox_start_time ))
+  local elapsed_time=$(( SECONDS - _prompt_paradox_start_time ))
   local hours minutes seconds remainder
 
   print -Pn "\n"
-  if (( end_time >= 3600 )); then
-    hours=$(( end_time / 3600 ))
-    remainder=$(( end_time % 3600 ))
+  print -Pn "%B%F{blue}>>> elapsed time%b "
+  if (( elapsed_time >= 3600 )); then
+    hours=$(( elapsed_time / 3600 ))
+    remainder=$(( elapsed_time % 3600 ))
     minutes=$(( remainder / 60 ))
     seconds=$(( remainder % 60 ))
-    print -Pn "%B%F{red}>>> elapsed time ${hours}h${minutes}m${seconds}s%b"
-  elif (( end_time >= 60 )); then
-    minutes=$(( end_time / 60 ))
-    seconds=$(( end_time % 60 ))
-    print -Pn "%B%F{yellow}>>> elapsed time ${minutes}m${seconds}s%b"
-  elif (( end_time > 10 )); then
-    print -Pn "%B%F{green}>>> elapsed time ${end_time}s%b"
+    print -Pn "%B%F{red}${hours}h${minutes}m${seconds}s%b"
+  elif (( elapsed_time >= 60 )); then
+    minutes=$(( elapsed_time / 60 ))
+    seconds=$(( elapsed_time % 60 ))
+    print -Pn "%B%F{yellow}${minutes}m${seconds}s%b"
+  elif (( elapsed_time > 0 )); then
+    print -Pn "%B%F{green}${elapsed_time}s%b"
   else
-    print -Pn "%B%F{green}>>>"
+    print -Pn "%B%F{green}<1s%b"
   fi
-  print -P " %F{blue}finished at: [%F{green}%D{%H:%M:%S}%F{blue}]%f"
+  print -P " %F{blue}finished at: %F{green}%D{%H:%M:%S}%F{blue}%f"
 }
