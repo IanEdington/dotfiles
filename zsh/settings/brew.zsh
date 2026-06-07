ed048e7474c045e882580e3359383fa878df0837
Ian Edington <ian.edington@gmail.com>
2026-06-06 23:48:53 -0400

    fix node version pulling from nvm

--- zsh/settings/brew.zsh
+++ zsh/settings/brew.zsh
@@ -5,5 +5,13 @@ export HOMEBREW_NO_INSECURE_REDIRECT=1
 
 export HOMEBREW_CASK_OPTS=--require-sha
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# instead of echo "$(/opt/homebrew/bin/brew shellenv)"
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+fpath[1,0]="/opt/homebrew/share/zsh/site-functions";
+export FPATH;
+# eval "$(/usr/bin/env PATH_HELPER_ROOT="/opt/homebrew" /usr/libexec/path_helper -s)"
+[ -z "${MANPATH-}"  ] || export MANPATH=":${MANPATH#:}";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 
